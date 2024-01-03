7684672ddd913d7d18d15b64af993b9d21f2d1ab
Thiaggo Ferreira <thiaggo98@gmail.com>
2024-01-03 10:23:15 -0300

    Implementação do cache finalizada

---
+++ src/infra/cache/redis/redis.service.ts
@@ -0,0 +1,18 @@
+﻿import { EnvService } from '@/infra/env/env.service'
+import { Injectable, OnModuleDestroy } from '@nestjs/common'
+import { Redis } from 'ioredis'
+
+@Injectable()
+export class RedisService extends Redis implements OnModuleDestroy {
+  constructor(envService: EnvService) {
+    super({
+      port: envService.get('REDIS_PORT'),
+      host: envService.get('REDIS_HOST'),
+      db: envService.get('REDIS_DB'),
+    })
+  }
+
+  onModuleDestroy() {
+    return this.disconnect()
+  }
+}
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -3,18 +3,27 @@ import { PrismaClient } from '@prisma/client'
 import { config } from 'dotenv'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
+import { Redis } from 'ioredis'
+import { envSchema } from '@/infra/env/env'
+
+const env = envSchema.parse(process.env)
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('DATABASE_URL environment variable must be set')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
   return url.toString()
@@ -25,9 +34,11 @@ const schemaId = randomUUID()
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
 
-  process.env.DATABASE_URL = databaseURL
+  env.DATABASE_URL = databaseURL
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('pnpm prisma migrate deploy')
 })
